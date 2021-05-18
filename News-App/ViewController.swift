e77437b39077ec8e612a9bf8922195cb243edbc0
edo Lorenza <edolrnz@gmail.com>
2021-05-18 18:33:03 +0700

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by Edo Lorenza on 18/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
